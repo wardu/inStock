e1f97eaa395c6a0aa88c18a32121b4209cdbdc69
Pauline Jeremie <pauline.jeremie@gmail.com>
2022-10-17 21:55:41 +0100

    Create all routes and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { BrowserRouter } from "react-router-dom";
+import AddItem from "./components/AddItem/AddItem";
+import AddWarehouse from "./components/AddWarehouse/AddWarehouse";
+import EditItem from "./components/EditItem/EditItem";
+import EditWarehouse from "./components/EditWarehouse/EditWarehouse";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import ItemDetails from "./components/ItemDetails/ItemDetails";
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+import Warehouses from "./components/Warehouses/Warehouses";
+
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/warehouses" element={<Warehouses />} />
+          <Route
+            path="/warehouses/:warehouseId"
+            element={<WarehouseDetails />}
+          />
+          <Route
+            path="/warehouses/:warehouseId/edit"
+            element={<EditWarehouse />}
+          />
+          <Route path="/warehouses/add" element={<AddWarehouse />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/inventory/:itemId" element={<ItemDetails />} />
+          <Route path="/inventory/:itemId/edit" element={<EditItem />} />
+          <Route path="/inventory/add" element={<AddItem />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
