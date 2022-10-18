898b12310a3c9d38185deb97c380428731bfa969
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-18 10:53:11 +0100

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo/InStock-Logo.svg";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <div className="header__wrapper">
+        <div>
+          <img src={Logo} alt="instock logo" />
+        </div>
+        <div className="header__container">
+          <p className="header__item">Warehouses</p>
+          <p className="header__item">Inventory</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
