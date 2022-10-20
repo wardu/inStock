d888941f02433b4e684433a1c75aed41830678cf
Pauline Jeremie <pauline.jeremie@gmail.com>
2022-10-20 18:03:34 +0100

    Create modal logic and style for mobile

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteItemModal from "../DeleteItemModal/DeleteItemModal";
 
 const Inventory = () => {
-  return <div>Inventory</div>;
+  const [showModal, setShowModal] = useState(false);
+
+  const showDeleteModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const selectedItem = {
+    id: "9abbf2d2-45d5-463c-8429-ca5454d971d4",
+    warehouseID: "bfc9bea7-66f1-44e9-879b-4d363a888eb4",
+    warehouseName: "San Fran",
+    itemName: "Water Bottle",
+    description:
+      "With a 1-litre capacity and BPA-free, this water-bottle is perfect for long days out.",
+    category: "Accessories",
+    status: "In Stock",
+    quantity: 9875,
+  };
+
+  return (
+    <div>
+      Inventory
+      <button
+        onClick={() => {
+          showDeleteModal();
+        }}
+      >
+        Delete item
+      </button>
+      <div>
+        {showModal && (
+          <DeleteItemModal
+            selectedItem={selectedItem}
+            showDeleteModal={showDeleteModal}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Inventory;
