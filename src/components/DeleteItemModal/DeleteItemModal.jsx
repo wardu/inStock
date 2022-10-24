9fa0b9327c2336c26d149347d37f38ab49d10443
Warren Dubery <warrendubery@protonmail.ch>
2022-10-24 22:36:53 +0100

    Clean up delete modals

--- src/components/DeleteItemModal/DeleteItemModal.jsx
+++ src/components/DeleteItemModal/DeleteItemModal.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import CrossIcon from "../../assets/icons/close-24px.svg";
-import Button from "../Button/Button";
-import "./DeleteItemModal.scss";
+import React from 'react';
+import CrossIcon from '../../assets/icons/close-24px.svg';
+import Button from '../Button/Button';
+import './DeleteItemModal.scss';
 
 const DeleteItemModal = ({
   selectedItem,
@@ -10,47 +10,41 @@ const DeleteItemModal = ({
   list,
   description,
 }) => {
-  //   const deleteItem = async (id) => {
-  //     await axios.delete(`http://localhost:8080/inventory/${id}`);
-  //     getInventories();
-  //     showDeleteModal();
-  //   };
-
   return (
-    <section className="delete-modal">
-      <div className="delete-modal__container">
-        <div className="delete-modal__icon-container">
+    <section className='delete-modal'>
+      <div className='delete-modal__container'>
+        <div className='delete-modal__icon-container'>
           <img
             src={CrossIcon}
-            alt="A cross icon"
+            alt='A cross icon'
             onClick={() => {
               showDeleteModal();
             }}
           />
         </div>
-        <div className="delete-modal__wrapper">
-          <div className="delete-modal__copy">
-            <h2 className="delete-modal__title">
+        <div className='delete-modal__wrapper'>
+          <div className='delete-modal__copy'>
+            <h2 className='delete-modal__title'>
               Delete {selectedItem} {description}?
             </h2>
-            <p className="delete-modal__text">
-              Please confirm that you'd like to delete {selectedItem} from the{" "}
+            <p className='delete-modal__text'>
+              Please confirm that you'd like to delete {selectedItem} from the{' '}
               {list}. You won't be able to undo this action.
             </p>
           </div>
 
-          <div className="delete-modal__buttons">
+          <div className='delete-modal__buttons'>
             <Button
-              buttonClass={"button button--cancel"}
+              buttonClass={'button button--cancel'}
               buttonAction={() => {
                 showDeleteModal();
               }}
-              buttonText="Cancel"
+              buttonText='Cancel'
             />
             <Button
-              buttonClass={"button button--delete"}
+              buttonClass={'button button--delete'}
               buttonAction={() => deleteItem()}
-              buttonText="Delete"
+              buttonText='Delete'
             />
           </div>
         </div>
--- src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
+++ src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
@@ -1,60 +1,11 @@
-import "./DeleteWarehouseModal.scss";
+import './DeleteWarehouseModal.scss';
 
 const DeleteWarehouseModal = ({
   selectedWarehouse,
   showDeleteModal,
   getWarehouses,
 }) => {
-  // const deleteWarehouse = async (id) => {
-  //   await axios.delete(`http://localhost:8080/warehouses/${id}`);
-  //   getWarehouses();
-  //   showDeleteModal();
-  // };
-
-  return (
-    <section className="delete-modal">
-      {/* <div className="delete-modal__container">
-        <div className="delete-modal__icon-container">
-          <img
-            src={CrossIcon}
-            alt="A cross icon"
-            onClick={() => {
-              showDeleteModal();
-            }}
-          />
-        </div>
-        <div className="delete-modal__wrapper">
-          <div className="delete-modal__copy">
-            <h2 className="delete-modal__title">
-              Delete {selectedWarehouse.city} warehouse?
-            </h2>
-            <p className="delete-modal__text">
-              Please confirm that you'd like to delete the{" "}
-              {selectedWarehouse.city} from the list of warehouses. You won't be
-              able to undo this action.
-            </p>
-          </div>
-
-          <div className="delete-modal__buttons">
-            <button
-              className="delete-modal__button delete-modal__button--cancel"
-              onClick={() => {
-                showDeleteModal();
-              }}
-            >
-              Cancel
-            </button>
-            <button
-              className="delete-modal__button delete-modal__button--delete"
-              onClick={() => deleteWarehouse(selectedWarehouse)}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </div> */}
-    </section>
-  );
+  return <section className='delete-modal'></section>;
 };
 
 export default DeleteWarehouseModal;
